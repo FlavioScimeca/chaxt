f0435d18c4ce756c9f6c21fdb4da0e3c4c16f63c
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-05-18 00:30:51 +0200

    bug deploy 1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,14 +5,14 @@ import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-import prisma from '@/app/libs/prismadb';
+import prisma from '../../../libs/prismadb';
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -26,7 +26,7 @@ export const authOptions: AuthOptions = {
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error('Invalid Credentials');
+          throw new Error('Invalid credentials');
         }
 
         const user = await prisma.user.findUnique({
@@ -43,15 +43,16 @@ export const authOptions: AuthOptions = {
           credentials.password,
           user.hashedPassword
         );
+
         if (!isCorrectPassword) {
-          throw new Error('Invalid Credentials');
+          throw new Error('Invalid credentials');
         }
 
         return user;
       },
     }),
   ],
-  debug: process.env.NODE_ENV == 'development',
+  debug: process.env.NODE_ENV === 'development',
   session: {
     strategy: 'jwt',
   },
